ca209aeac17fce30c8b0eae38e0bc2cb7f300737
Dheeraj Arora <dheeraj.a@nopaperforms.com>
2022-06-02 23:37:49 +0530

    component added

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+
+export default function Landing() {
+  return (
+    <>
+    <h1 class="bg-primary">Hello world !</h1>
+    </>
+  )
+}
